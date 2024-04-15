4ff3b94c07472cc64a017880832e60cce9dfac2d
prashantkumbhar2002 <prashantkumbhar5515@gmail.com>
2024-04-15 14:26:14 +0530

    Using axios as client to fetch data from API

--- redis-node/index.js
+++ redis-node/index.js
@@ -1,9 +1,17 @@
 // const express = require('express')
 import express from "express";
-
+import axios from "axios"
 const app = express();
 
 const PORT = process.env.PORT || 3005;
+
+async function getApiData(species){
+    const apiRes = await axios.get(`https://www.fishwatch.gov/api/species/${species}`);
+    console.log("Request sent to API ")
+    return apiRes.data;
+}
+
+
 
 app.listen(PORT, ()=> {
   console.log(`⚙️ Server is running at port : ${PORT}`)
