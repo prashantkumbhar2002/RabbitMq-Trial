b0911db41ef83f481ae9b64a2e2a8640a4798dac
prashantkumbhar2002 <prashantkumbhar5515@gmail.com>
2024-04-15 14:30:40 +0530

    added the route and handler to fetch the data

--- redis-node/index.js
+++ redis-node/index.js
@@ -5,14 +5,32 @@ const app = express();
 
 const PORT = process.env.PORT || 3005;
 
-async function getApiData(species){
+async function getApiData(species) {
     const apiRes = await axios.get(`https://www.fishwatch.gov/api/species/${species}`);
     console.log("Request sent to API ")
     return apiRes.data;
 }
 
 
+async function getSpeciesData(req, res) {
+    const { species } = req.params;
+    let results;
+    try {
+        results = await getApiData(species);
+        if (results.length === 0) {
+            throw "API returned an empty array";
+        }
+        res.send({
+            fromCache: false,
+            data: results,
+        });
+    } catch (error) {
+        console.error(error);
+        res.status(404).send("Data unavailable");
+    }
+}
 
-app.listen(PORT, ()=> {
-  console.log(`⚙️ Server is running at port : ${PORT}`)
+app.get("/fish/:species", getSpeciesData);
+app.listen(PORT, () => {
+    console.log(`⚙️ Server is running at port : ${PORT}`)
 })
